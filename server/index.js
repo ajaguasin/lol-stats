b685141dd10ad30ac03cfb1c7378b8e6b1e73a82
aj <ajaguasin@gmail.com>
2019-03-10 16:09:44 -0700

    Set up build scripts

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 8000;
 const app = express();
 const riotAPI = require("./api/riotAPI");
 
+if (process.env.NODE_ENV === "production") {
+  const root = path.resolve(__dirname, "../public");
+  app.use(express.static(root));
+  app.use(fallback("index.html", { root }));
+}
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
