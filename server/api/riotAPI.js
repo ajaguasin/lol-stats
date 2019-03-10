a3aaed67f6f8b4a276f1cc9e84b8e0f155a45cae
aj <ajaguasin@gmail.com>
2019-03-10 15:14:03 -0700

    Get my match details

--- server/api/riotAPI.js
+++ server/api/riotAPI.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const { Kayn, REGIONS } = require("kayn");
 const API_KEY = process.env.API_KEY;
 
@@ -33,7 +34,17 @@ const MATCH_LIST_URL = "/lol/match/v4/matchlists/by-account/";
 
 router.get("/", async (req, res, next) => {
   const { summonerName } = req.query;
-  getSummonerByName(summonerName).then(res => console.log(res));
+
+  const user = await getSummonerByName(summonerName);
+  //   console.log(user.accountId);
+
+  const matchList = await getMatchListByAccount(user.accountId);
+  //   console.log(matchList);
+
+  const matchList_20 = get20Matches(matchList);
+  //   console.log(matchList_20);
+
+  const match = await getMyMatchDetails(matchList_20[0], summonerName);
 });
 
 async function getSummonerByName(summonerName) {
@@ -41,6 +52,31 @@ async function getSummonerByName(summonerName) {
   return summoner;
 }
 
-async function getMatchListByAccount(accountId) {}
+async function getMatchListByAccount(accountId) {
+  const matchList = await kayn.MatchlistV4.by.accountID(accountId);
+  return matchList;
+}
+
+async function getMyMatchDetails(matchID, summonerName) {
+  const match = await kayn.Match.get(matchID);
+  const myParticipantId = match.participantIdentities.filter(participants => {
+    return participants.player.summonerName === summonerName;
+  });
+
+  const myDetails = match.participants.filter(participants => {
+    return participants.participantId === myParticipantId[0].participantId;
+  });
+
+  console.log(myDetails[0]);
+}
+
+function get20Matches(matchList) {
+  let matches = [];
+
+  for (let i = 0; i < 10; i++) {
+    matches.push(matchList.matches[i].gameId);
+  }
+  return matches;
+}
 
 module.exports = router;
